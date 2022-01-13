8970213c7f37f11608573d5d0b36ba9a167c0d08
Agustin Kassis <webmaster@masize.com>
2022-01-13 16:10:47 -0300

    updated images and animations

--- app/constants/network.js
+++ app/constants/network.js
@@ -1,5 +1,7 @@
-export const MAINNET = 'rpc';
-export const ETHEREUM = 'mainnet';
-export const BSC = 'rpc';
+export const MAINNET = 'mainnet';
+export const ROPSTEN = 'ropsten';
+export const KOVAN = 'kovan';
+export const RINKEBY = 'rinkeby';
+export const GOERLI = 'goerli';
 export const RPC = 'rpc';
 export const NO_RPC_BLOCK_EXPLORER = 'NO_BLOCK_EXPLORER';
